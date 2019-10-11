64610429f96388a817481a44966fd5d3971e854f
Kishan <kishangupta21995@gmail.com>
2019-10-10 22:56:09 -0400

    Initial commit

---
+++ app/src/main/java/com/example/barncodemoapplication/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.barncodemoapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+
+
+
+    }
+}
